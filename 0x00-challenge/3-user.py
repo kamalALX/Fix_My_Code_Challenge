a803b6049e9063b27c7238ee7fcca609bba9d68c
kamal <kamalmiftah01@gmail.com>
2024-03-21 02:19:40 +0000

    Solving task 3

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -54,7 +54,6 @@ class User():
             return False
         if self.__password is None:
             return False
-        return hashlib.md5(pwd.encode()).hexdigest().lower() == self.__password
 
 
 if __name__ == '__main__':
