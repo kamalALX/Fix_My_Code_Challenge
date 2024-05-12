ed733666222a6338440ea6564c07ad85e290f9b7
Kamal <kamalmiftah01@gmail.com>
2024-05-12 21:46:11 +0100

    task 1

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -25,7 +25,7 @@ class square():
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
-
+    """ class square that creates a square """
     s = square(width=12, height=12)
     print(s)
     print(s.area_of_my_square())
