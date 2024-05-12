f96330354416e9558a5f6ed5ad11b6af282dadee
Kamal <kamalmiftah01@gmail.com>
2024-05-12 21:54:35 +0100

    solving task 1

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -4,17 +4,17 @@
 
 class Square():
     """ class square that creates a square """
-    
+   width = 0
+   height = 0
+
     def __init__(self, *args, **kwargs):
         """ class square that creates a square """
-        self.width = 0
-        self.height = 0
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
     def perimeter_of_my_square(self):
         """ class square that creates a square """
@@ -25,7 +25,7 @@ class Square():
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
-    s = square(width=12, height=12)
+    s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.perimeter_of_my_square())
